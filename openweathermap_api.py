906e4a8aa7ebf564c027f0c891680df9ebb7a091
Toomore Chiang <toomore0929@gmail.com>
2014-03-24 02:16:07 +0800

    Add `get_forecast`.

--- openweathermap_api.py
+++ openweathermap_api.py
@@ -8,25 +8,30 @@ class OpenWeatherMapAPI(object):
     VERSION = '2.5'
     API_URL = 'http://api.openweathermap.org/'
     WEATHER_URL = urljoin(API_URL, 'data/%s/weather' % VERSION)
+    FORECAST_URL = urljoin(API_URL, 'data/%s/forecast' % VERSION)
 
     def __init__(self, appid):
         self.appid = appid
 
     def _requests(self, path, **kwargs):
-        result = requests.get(path, params=kwargs,
-                headers={'x-api-key': self.appid})
-        return result.json()
-
-    def get_weather(self, **kwargs):
         if 'units' not in kwargs:
             kwargs['units'] = 'metric'
 
         if 'lang' not in kwargs:
             kwargs['lang'] = 'zh_tw'
 
+        result = requests.get(path, params=kwargs,
+                headers={'x-api-key': self.appid})
+        return result.json()
+
+    def get_weather(self, **kwargs):
         return self._requests(self.WEATHER_URL, **kwargs)
+
+    def get_forecast(self, **kwargs):
+        return self._requests(self.FORECAST_URL, **kwargs)
 
 if __name__ == '__main__':
     from pprint import pprint
     #pprint(OpenWeatherMapAPI(setting.APPID).get_weather(q='kaohsiung'))
     pprint(OpenWeatherMapAPI(setting.APPID).get_weather(id=1673820))
+    pprint(OpenWeatherMapAPI(setting.APPID).get_forecast(id=1673820))
