f39bc3c16ce613d8195b035e0e3754f4b136139e
Toomore Chiang <toomore0929@gmail.com>
2014-03-24 00:00:51 +0800

    Add `get_weather`.

--- openweathermap_api.py
+++ openweathermap_api.py
@@ -1,13 +1,20 @@
 # -*- coding:utf8 -*-
 import requests
+from urlparse import urljoin
+
 
 class OpenWeatherMapAPI(object):
+    VERSION = '2.5'
+    API_URL = 'http://api.openweathermap.org/'
+    WEATHER_URL = urljoin(API_URL, 'data/%s/weather' % VERSION)
 
     def _requests(self, path, **kwargs):
         result = requests.get(path, params=kwargs)
         return result.json()
 
+    def get_weather(self, **kwargs):
+        return self._requests(self.WEATHER_URL, **kwargs)
+
 if __name__ == '__main__':
     from pprint import pprint
-    pprint(OpenWeatherMapAPI()._requests('http://api.openweathermap.org/data/2.5/weather',
-            q='kaohsiung'))
+    pprint(OpenWeatherMapAPI().get_weather(q='kaohsiung'))
