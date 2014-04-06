f1c2c67046f94c1215e661b5a6614d46b80b5d19
Toomore Chiang <toomore0929@gmail.com>
2014-04-07 01:31:00 +0800

    To be classmethod.

--- openweathermap_api.py
+++ openweathermap_api.py
@@ -11,11 +11,13 @@ HISTORY_URL = urljoin(API_URL, 'data/%s/history/city' % VERSION)
 
 
 class OpenWeatherMapAPI(object):
+    APPID = ''
 
     def __init__(self, appid):
-        self.appid = appid
+        OpenWeatherMapAPI.APPID = appid
 
-    def _requests(self, path, **kwargs):
+    @classmethod
+    def _requests(cls, path, **kwargs):
         if 'units' not in kwargs:
             kwargs['units'] = 'metric'
 
@@ -23,31 +25,36 @@ class OpenWeatherMapAPI(object):
             kwargs['lang'] = 'zh_tw'
 
         result = requests.get(path, params=kwargs,
-                headers={'x-api-key': self.appid})
+                headers={'x-api-key': cls.APPID})
+        print cls.APPID
         return result.json()
 
-    def get_weather(self, **kwargs):
+    @classmethod
+    def get_weather(cls, **kwargs):
         '''Get current weather data.
            wiki: http://bugs.openweathermap.org/projects/api/wiki/Api_2_5_weather
         '''
-        return self._requests(WEATHER_URL, **kwargs)
+        return cls._requests(WEATHER_URL, **kwargs)
 
-    def get_forecast(self, **kwargs):
+    @classmethod
+    def get_forecast(cls, **kwargs):
         ''' Get forecast data.
             wiki: http://bugs.openweathermap.org/projects/api/wiki/Api_2_5_forecast
         '''
-        return self._requests(FORECAST_URL, **kwargs)
+        return cls._requests(FORECAST_URL, **kwargs)
 
-    def get_history(self, **kwargs):
+    @classmethod
+    def get_history(cls, **kwargs):
         ''' Get city History
             wiki: http://bugs.openweathermap.org/projects/api/wiki/Api_2_5_history
         '''
-        return self._requests(HISTORY_URL, **kwargs)
+        return cls._requests(HISTORY_URL, **kwargs)
 
 if __name__ == '__main__':
     from pprint import pprint
     #pprint(OpenWeatherMapAPI(setting.APPID).get_weather(q='kaohsiung'))
     openweathermapapi = OpenWeatherMapAPI(setting.APPID)
     pprint(openweathermapapi.get_weather(id=1673820))
+    pprint(OpenWeatherMapAPI.get_weather(id=1673820))
     pprint(openweathermapapi.get_forecast(id=1673820))
     pprint(openweathermapapi.get_history(id=1673820, type='hour'))
